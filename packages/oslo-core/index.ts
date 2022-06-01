4edbc29efeb42cc9b3de91b657ff99c5dfbba2bc
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2022-06-01 11:29:08 +0200

    Remove runner interface from core package

--- packages/oslo-core/index.ts
+++ packages/oslo-core/index.ts
@@ -6,6 +6,5 @@ export * from './lib/interfaces/Converter';
 export * from './lib/interfaces/Generator';
 export * from './lib/interfaces/OutputHandler';
 export * from './lib/utils/namespaces';
-export * from './lib/interfaces/Runner';
 export * from './lib/oslo/Scope';
 export * from './lib/oslo/PropertyType';
