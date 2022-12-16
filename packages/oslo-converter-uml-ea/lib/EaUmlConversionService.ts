012f44911c898f706f75d42f155814e2d8ff7ae1
Dwight Van Lancker <dwight.vanlancker@hotmail.com>
2022-12-16 10:40:55 +0100

    Add context + document information to JSON-LD + remove comments

--- packages/oslo-converter-uml-ea/lib/EaUmlConversionService.ts
+++ packages/oslo-converter-uml-ea/lib/EaUmlConversionService.ts
@@ -1,9 +1,11 @@
 /* eslint-disable eslint-comments/disable-enable-pair */
 /* eslint-disable @typescript-eslint/indent */
-import { Logger } from '@oslo-flanders/core';
+import { Logger, ns } from '@oslo-flanders/core';
 import type { IConversionService } from '@oslo-flanders/core';
 import { DataRegistry } from '@oslo-flanders/ea-uml-extractor';
 import { inject, injectable } from 'inversify';
+import type { Store, Quad } from 'n3';
+import { DataFactory } from 'rdf-data-factory';
 import { EaUmlConverterConfiguration } from './config/EaUmlConverterConfiguration';
 import { EaUmlConverterServiceIdentifier } from './config/EaUmlConverterServiceIdentifier';
 import { ConverterHandlerService } from './ConverterHandlerService';
@@ -35,8 +37,23 @@ export class EaUmlConversionService implements IConversionService {
     const store = await converterHandler.filterIgnoredObjects(model)
       .then(() => converterHandler.normalize(model))
       .then(() => converterHandler.assignUris(model))
-      .then(uriRegistry => converterHandler.convert(model, uriRegistry));
+      .then(uriRegistry => converterHandler.convert(model, uriRegistry))
+      .then(_store => this.addDocumentInformation(_store));
 
     await this.outputHandlerService.write(store);
+  }
+
+  private async addDocumentInformation(store: Store): Promise<Store<Quad>> {
+    const df = new DataFactory();
+    const versionUri = `${this.configuration.baseUri}/${this.configuration.versionId}`;
+
+    // The output handler will use this quad to set the id of the document
+    store.addQuad(
+      df.namedNode(versionUri),
+      ns.prov('generatedAtTime'),
+      df.literal(new Date(Date.now()).toISOString(), ns.xsd('datetime')),
+    );
+
+    return <Store<Quad>>store;
   }
 }
