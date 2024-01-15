c57b226f6222f7dd34cf05d075c473f217b6dffd
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2024-01-15 17:42:07 +0100

    Added `module-alias` so Node can recognize the aliases

--- packages/oslo-converter-uml-ea/bin/runner.ts
+++ packages/oslo-converter-uml-ea/bin/runner.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+require('module-alias/register');
 import { EaUmlConversionServiceRunner } from '../lib/EaUmlConversionServiceRunner';
 // eslint-disable-next-line no-sync
 new EaUmlConversionServiceRunner().runCliSync(process);
