319104b56b37f3beb779f12a9d20db245dfe9bfe
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2024-05-16 10:22:17 +0200

    Removed build files

--- constants/enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SpecificationType = void 0;
-var SpecificationType;
-(function (SpecificationType) {
-    SpecificationType["ap"] = "ApplicationProfile";
-    SpecificationType["voc"] = "Vocabulary";
-})(SpecificationType || (exports.SpecificationType = SpecificationType = {}));
-//# sourceMappingURL=enum.js.map
