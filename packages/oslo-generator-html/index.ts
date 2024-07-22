70740fc31edd11ec8cd47a194a95ce76409a784f
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2024-07-22 09:46:52 +0200

    Added `index.ts` to package `oslo-generator-html`

---
+++ packages/oslo-generator-html/index.ts
@@ -0,0 +1,10 @@
+import 'reflect-metadata';
+
+export * from './lib/HtmlGenerationService';
+export * from './lib/HtmlGenerationServiceRunner';
+export * from './lib/config/DependencyInjectionConfig';
+export * from './lib/config/HtmlGenerationServiceConfiguration';
+export * from './lib/utils/utils';
+export * from './lib/utils/languageEnum';
+export * from './lib/utils/specificationTypeEnum';
+export * from './lib/types/Class';
