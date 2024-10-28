9bd078ff0230ebeadf196e01f9aa5470b32a6e68
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2024-10-28 17:38:03 +0100

    Update tests to work with renewed `applyFiltering`

--- packages/oslo-generator-json-webuniversum/test/JsonWebuniversumGenerationService.unit.test.ts
+++ packages/oslo-generator-json-webuniversum/test/JsonWebuniversumGenerationService.unit.test.ts
@@ -86,6 +86,29 @@ describe('JsonWebuniversumGenerationServiceConfiguration', () => {
       JSON.stringify(
         {
           baseURI: 'http://example.org/ns/domain#',
+          entities: [
+            {
+              id: 'http://example.org/id/class/1',
+              vocabularyLabel: {
+                en: 'VocTestClass',
+              },
+              applicationProfileLabel: {
+                en: 'ApTestClass',
+              },
+              vocabularyDefinition: {
+                en: 'VocDefinition',
+              },
+              applicationProfileDefinition: {
+                en: 'ApDefinition',
+              },
+              vocabularyUsageNote: {
+                en: 'VocUsageNote',
+              },
+              applicationProfileUsageNote: {
+                en: 'ApUsageNote',
+              },
+            },
+          ],
           classes: [
             {
               id: 'http://example.org/id/class/1',
@@ -132,6 +155,7 @@ describe('JsonWebuniversumGenerationServiceConfiguration', () => {
               },
             },
           ],
+          properties: [],
         },
         null,
         2,
