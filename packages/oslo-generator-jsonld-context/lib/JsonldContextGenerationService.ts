617f3fdcaa26eef586913491f89766376bf2a746
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2025-01-13 18:13:46 +0100

    Made the `context-generator` look at the value of the URI. If the label and URI are identical, don't complain about duplicate labels

--- packages/oslo-generator-jsonld-context/lib/JsonldContextGenerationService.ts
+++ packages/oslo-generator-jsonld-context/lib/JsonldContextGenerationService.ts
@@ -7,14 +7,12 @@ import {
   ns,
   ServiceIdentifier,
   QuadStore,
-  getApplicationProfileLabel
+  getApplicationProfileLabel,
 } from '@oslo-flanders/core';
 
 import type * as RDF from '@rdfjs/types';
 import { inject, injectable } from 'inversify';
-import {
-  JsonldContextGenerationServiceConfiguration,
-} from './config/JsonldContextGenerationServiceConfiguration';
+import { JsonldContextGenerationServiceConfiguration } from './config/JsonldContextGenerationServiceConfiguration';
 import { toCamelCase, toPascalCase } from './utils/utils';
 import { ClassMetadata } from './types/ClassMetadata';
 import { PropertyMetadata } from './types/PropertyMetadata';
@@ -27,7 +25,8 @@ export class JsonldContextGenerationService implements IService {
 
   public constructor(
     @inject(ServiceIdentifier.Logger) logger: Logger,
-    @inject(ServiceIdentifier.Configuration) config: JsonldContextGenerationServiceConfiguration,
+    @inject(ServiceIdentifier.Configuration)
+    config: JsonldContextGenerationServiceConfiguration,
     @inject(ServiceIdentifier.QuadStore) store: QuadStore,
   ) {
     this.logger = logger;
@@ -57,10 +56,11 @@ export class JsonldContextGenerationService implements IService {
     const [classMetadata, propertyMetadata] = await Promise.all([
       this.createClassMetadata(),
       this.createPropertyMetadata(),
-    ])
+    ]);
 
-    return this.configuration.scopedContext ?
-      this.createScopedContext(classMetadata, propertyMetadata) : this.createRegularContext(classMetadata, propertyMetadata);
+    return this.configuration.scopedContext
+      ? this.createScopedContext(classMetadata, propertyMetadata)
+      : this.createRegularContext(classMetadata, propertyMetadata);
   }
 
   /**
@@ -71,8 +71,12 @@ export class JsonldContextGenerationService implements IService {
   private identifyDuplicateLabels(uris: RDF.NamedNode[]): RDF.NamedNode[] {
     const labelUriMap: Map<string, RDF.NamedNode[]> = new Map();
 
-    uris.forEach(uri => {
-      const label: RDF.Literal | undefined = getApplicationProfileLabel(uri, this.store, this.configuration.language);
+    uris.forEach((uri) => {
+      const label: RDF.Literal | undefined = getApplicationProfileLabel(
+        uri,
+        this.store,
+        this.configuration.language,
+      );
 
       if (!label) {
         return;
@@ -85,9 +89,21 @@ export class JsonldContextGenerationService implements IService {
 
     const duplicates: RDF.NamedNode[] = [];
     labelUriMap.forEach((subjects: RDF.NamedNode[], label: string) => {
-      const unique: Set<RDF.NamedNode> = new Set(subjects);
-      if (unique.size > 1) {
-        duplicates.push(...Array.from(unique));
+      const uniqueUris: Set<string> = new Set();
+      const uniqueSubjects: RDF.NamedNode[] = [];
+
+      // Check if the label is used multiple times for different URIs and add them to the duplicates array
+      // if the URI is the same for all subjects, only add the first one
+      subjects.forEach((subject) => {
+        const assignedUri = this.store.getAssignedUri(subject);
+        if (assignedUri && !uniqueUris.has(assignedUri.value)) {
+          uniqueUris.add(assignedUri.value);
+          uniqueSubjects.push(subject);
+        }
+      });
+
+      if (uniqueSubjects.length > 1) {
+        duplicates.push(...uniqueSubjects);
       }
     });
 
@@ -100,7 +116,10 @@ export class JsonldContextGenerationService implements IService {
    * @param propertyMetadata An array of PropertyMetadata objects
    * @returns A scoped context object
    */
-  private createScopedContext(classMetadata: ClassMetadata[], propertyMetadata: PropertyMetadata[]): any {
+  private createScopedContext(
+    classMetadata: ClassMetadata[],
+    propertyMetadata: PropertyMetadata[],
+  ): any {
     const result = classMetadata
       .sort((a, b) => a.label.value.localeCompare(b.label.value))
       .reduce((main, x: ClassMetadata) => {
@@ -110,26 +129,34 @@ export class JsonldContextGenerationService implements IService {
             '@id': x.assignedURI.value,
             '@context': {
               ...propertyMetadata
-                .filter((y: PropertyMetadata) => y.domainLabel.value === x.label.value)
+                .filter(
+                  (y: PropertyMetadata) =>
+                    y.domainLabel.value === x.label.value,
+                )
                 .sort((a, b) => a.label.value.localeCompare(b.label.value))
                 .reduce((subMain, y: PropertyMetadata) => {
                   return {
                     ...subMain,
                     [toCamelCase(y.label.value)]: {
                       '@id': y.assignedURI.value,
-                      ...y.rangeAssignedUri && { '@type': y.rangeAssignedUri.value },
-                      ...y.addContainer === true && { '@container': '@set' },
-                    }
-                  }
-                }, {})
-            }
-          }
-        }
+                      ...(y.rangeAssignedUri && {
+                        '@type': y.rangeAssignedUri.value,
+                      }),
+                      ...(y.addContainer === true && { '@container': '@set' }),
+                    },
+                  };
+                }, {}),
+            },
+          },
+        };
       }, {});
 
     // Delete empty @context objects
     for (const [key, value] of Object.entries(result)) {
-      if (typeof value === 'object' && Object.keys((<any>value)['@context']).length === 0) {
+      if (
+        typeof value === 'object' &&
+        Object.keys((<any>value)['@context']).length === 0
+      ) {
         delete (<any>result)[key]['@context'];
       }
     }
@@ -143,26 +170,37 @@ export class JsonldContextGenerationService implements IService {
    * @param propertyMetadata An array of PropertyMetadata objects
    * @returns A regular context object
    */
-  private createRegularContext(classMetadata: ClassMetadata[], propertyMetadata: PropertyMetadata[]): any {
-    const classContext = Object.fromEntries(classMetadata
-      .sort((a, b) => a.label.value.localeCompare(b.label.value))
-      .map((x: ClassMetadata) => [x.label.value, x.assignedURI.value]))
+  private createRegularContext(
+    classMetadata: ClassMetadata[],
+    propertyMetadata: PropertyMetadata[],
+  ): any {
+    const classContext = Object.fromEntries(
+      classMetadata
+        .sort((a, b) => a.label.value.localeCompare(b.label.value))
+        .map((x: ClassMetadata) => [x.label.value, x.assignedURI.value]),
+    );
 
-
-    const propertyContext = propertyMetadata
-      .reduce((main, x: PropertyMetadata) => {
+    const propertyContext = propertyMetadata.reduce(
+      (main, x: PropertyMetadata) => {
         return {
           ...main,
-          [x.addPrefix ? `${toPascalCase(x.domainLabel.value)}.${toCamelCase(x.label.value)}` : toCamelCase(x.label.value)]: {
+          [x.addPrefix
+            ? `${toPascalCase(x.domainLabel.value)}.${toCamelCase(x.label.value)}`
+            : toCamelCase(x.label.value)]: {
             '@id': x.assignedURI.value,
-            ...x.rangeAssignedUri && { '@type': x.rangeAssignedUri.value },
-            ...x.addContainer === true && { '@container': '@set' },
-          }
-        }
-      }, {});
+            ...(x.rangeAssignedUri && { '@type': x.rangeAssignedUri.value }),
+            ...(x.addContainer === true && { '@container': '@set' }),
+          },
+        };
+      },
+      {},
+    );
 
-    return Object
-      .fromEntries(Object.entries({ ...classContext, ...propertyContext }).sort((a, b) => a[0].localeCompare(b[0])));
+    return Object.fromEntries(
+      Object.entries({ ...classContext, ...propertyContext }).sort((a, b) =>
+        a[0].localeCompare(b[0]),
+      ),
+    );
   }
 
   /**
@@ -174,21 +212,34 @@ export class JsonldContextGenerationService implements IService {
     const classSubjects: RDF.NamedNode[] = this.store.getClassIds();
     const duplicates = this.identifyDuplicateLabels(classSubjects);
 
-    classSubjects.forEach(subject => {
+    classSubjects.forEach((subject) => {
       try {
-        const label: RDF.Literal | undefined = getApplicationProfileLabel(subject, this.store, this.configuration.language);
+        const label: RDF.Literal | undefined = getApplicationProfileLabel(
+          subject,
+          this.store,
+          this.configuration.language,
+        );
         if (!label) {
-          throw new Error(`No label found for class ${subject.value} in language ${this.configuration.language}.`);
+          throw new Error(
+            `No label found for class ${subject.value} in language ${this.configuration.language}.`,
+          );
         }
 
         if (duplicates.includes(subject)) {
-          throw new Error(`Found ${subject.value} in duplicates, meaning "${label.value}" is used multiple times as label.`);
+          const uri: RDF.NamedNode | undefined =
+            this.store.getAssignedUri(subject);
+          throw new Error(
+            `Found ${subject.value} in duplicates, meaning "${label.value}" is used multiple times as label.`,
+          );
         }
 
-        const assignedUri: RDF.NamedNode | undefined = this.store.getAssignedUri(subject);
+        const assignedUri: RDF.NamedNode | undefined =
+          this.store.getAssignedUri(subject);
 
         if (!assignedUri) {
-          throw new Error(`Unable to find the assigned URI for class ${subject.value}.`);
+          throw new Error(
+            `Unable to find the assigned URI for class ${subject.value}.`,
+          );
         }
 
         classMetadata.push({
@@ -213,54 +264,81 @@ export class JsonldContextGenerationService implements IService {
     const datatypePropertySubjects = this.store.getDatatypePropertyIds();
     const objectPropertySubjects = this.store.getObjectPropertyIds();
 
-    const duplicates = this.identifyDuplicateLabels([...datatypePropertySubjects, ...objectPropertySubjects]);
-    [...datatypePropertySubjects, ...objectPropertySubjects].forEach(subject => {
-      try {
-        const assignedUri: RDF.NamedNode | undefined = this.store.getAssignedUri(subject);
+    const duplicates = this.identifyDuplicateLabels([
+      ...datatypePropertySubjects,
+      ...objectPropertySubjects,
+    ]);
+    [...datatypePropertySubjects, ...objectPropertySubjects].forEach(
+      (subject) => {
+        try {
+          const assignedUri: RDF.NamedNode | undefined =
+            this.store.getAssignedUri(subject);
 
-        if (!assignedUri) {
-          throw new Error(`Unable to find the assigned URI for attribute ${subject.value}.`);
+          if (!assignedUri) {
+            throw new Error(
+              `Unable to find the assigned URI for attribute ${subject.value}.`,
+            );
+          }
+
+          const label: RDF.Literal | undefined = getApplicationProfileLabel(
+            subject,
+            this.store,
+            this.configuration.language,
+          );
+          if (!label) {
+            throw new Error(
+              `No label found for attribute ${subject.value} in language "${this.configuration.language}" or without language tag.`,
+            );
+          }
+
+          const range: RDF.NamedNode | undefined = this.store.getRange(subject);
+          if (!range) {
+            throw new Error(`No range found for attribute ${subject.value}.`);
+          }
+          const rangeUri: RDF.NamedNode | undefined =
+            this.store.getAssignedUri(range);
+
+          if (!rangeUri) {
+            throw new Error(
+              `Unable to find the assigned URI of range with id ${range.value}.`,
+            );
+          }
+
+          const domain: RDF.NamedNode | undefined =
+            this.store.getDomain(subject);
+          if (!domain) {
+            throw new Error(`No domain found for attribute ${subject.value}.`);
+          }
+
+          const domainLabel: RDF.Literal | undefined =
+            getApplicationProfileLabel(
+              domain,
+              this.store,
+              this.configuration.language,
+            );
+
+          if (!domainLabel) {
+            throw new Error(
+              `No label found for domain ${domain.value} of attribute ${subject.value}.`,
+            );
+          }
+
+          propertyMetadata.push({
+            osloId: subject,
+            assignedURI: assignedUri,
+            label: label,
+            domainLabel: domainLabel,
+            rangeAssignedUri: rangeUri,
+            addContainer: this.canHaveAListOfValues(subject),
+            addPrefix:
+              this.configuration.addDomainPrefix ||
+              duplicates.includes(subject),
+          });
+        } catch (error) {
+          this.logger.error((<Error>error).message);
         }
-
-        const label: RDF.Literal | undefined = getApplicationProfileLabel(subject, this.store, this.configuration.language);
-        if (!label) {
-          throw new Error(`No label found for attribute ${subject.value} in language "${this.configuration.language}" or without language tag.`);
-        }
-
-        const range: RDF.NamedNode | undefined = this.store.getRange(subject);
-        if (!range) {
-          throw new Error(`No range found for attribute ${subject.value}.`);
-        }
-        const rangeUri: RDF.NamedNode | undefined = this.store.getAssignedUri(range);
-
-        if (!rangeUri) {
-          throw new Error(`Unable to find the assigned URI of range with id ${range.value}.`);
-        }
-
-        const domain: RDF.NamedNode | undefined = this.store.getDomain(subject);
-        if (!domain) {
-          throw new Error(`No domain found for attribute ${subject.value}.`);
-        }
-
-        const domainLabel: RDF.Literal | undefined = getApplicationProfileLabel(domain, this.store, this.configuration.language);
-
-        if (!domainLabel) {
-          throw new Error(`No label found for domain ${domain.value} of attribute ${subject.value}.`);
-        }
-
-        propertyMetadata.push({
-          osloId: subject,
-          assignedURI: assignedUri,
-          label: label,
-          domainLabel: domainLabel,
-          rangeAssignedUri: rangeUri,
-          addContainer: this.canHaveAListOfValues(subject),
-          addPrefix: this.configuration.addDomainPrefix || duplicates.includes(subject),
-        });
-      } catch (error) {
-        this.logger.error((<Error>error).message);
-      }
-    });
+      },
+    );
 
     return propertyMetadata;
   }
@@ -272,10 +350,13 @@ export class JsonldContextGenerationService implements IService {
    * @returns — A boolean indicating whether or not to add the "@container" property to the attribute
    */
   private canHaveAListOfValues(subject: RDF.Quad_Subject): boolean {
-    const maxCount: RDF.Literal | undefined = this.store.getMaxCardinality(subject);
+    const maxCount: RDF.Literal | undefined =
+      this.store.getMaxCardinality(subject);
 
     if (!maxCount) {
-      this.logger.warn(`Unable to retrieve max cardinality of property ${subject.value}.`);
+      this.logger.warn(
+        `Unable to retrieve max cardinality of property ${subject.value}.`,
+      );
       return false;
     }
 
