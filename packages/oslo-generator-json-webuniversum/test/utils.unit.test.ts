54054f0502c749b94868cb6008a4276ec5be78ef
Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
2025-02-20 18:04:32 +0100

    Use `SpecificationType` to define how to sort the overview

--- packages/oslo-generator-json-webuniversum/test/utils.unit.test.ts
+++ packages/oslo-generator-json-webuniversum/test/utils.unit.test.ts
@@ -2,40 +2,45 @@
  * @group unit
  */
 
-import { sortWebuniversumObjects } from "../lib/utils/utils";
+import { SpecificationType } from '@oslo-flanders/core';
+import { sortWebuniversumObjects } from '../lib/utils/utils';
 
 describe('Utils functions', () => {
-	it('should sort WebuniversumObjects based on their vocabulary label', () => {
-		const webuniversumObjects = [
-			{
-				id: 'http://example.com/A',
-				vocabularyLabel: {
-					en: 'A',
-				},
-			},
-			{
-				id: 'http://example.com/B',
-				vocabularyLabel: {
-					en: 'B',
-				},
-			},
-			{
-				id: 'http://example.com/C',
-				vocabularyLabel: {
-					en: 'C',
-				},
-			},
-			{
-				id: 'http://example.com/D',
-			},
-		];
+  it('should sort WebuniversumObjects based on their vocabulary label', () => {
+    const webuniversumObjects = [
+      {
+        id: 'http://example.com/A',
+        vocabularyLabel: {
+          en: 'A',
+        },
+      },
+      {
+        id: 'http://example.com/B',
+        vocabularyLabel: {
+          en: 'B',
+        },
+      },
+      {
+        id: 'http://example.com/C',
+        vocabularyLabel: {
+          en: 'C',
+        },
+      },
+      {
+        id: 'http://example.com/D',
+      },
+    ];
 
-		const sortedWebuniversumObjects = sortWebuniversumObjects(webuniversumObjects, 'en').map((obj) => obj.id);
-		expect(sortedWebuniversumObjects).toEqual([
-			'http://example.com/D',
-			'http://example.com/A',
-			'http://example.com/B',
-			'http://example.com/C',
-		])
-	});
-})
+    const sortedWebuniversumObjects = sortWebuniversumObjects(
+      webuniversumObjects,
+      SpecificationType.Vocabulary,
+      'en',
+    ).map((obj) => obj.id);
+    expect(sortedWebuniversumObjects).toEqual([
+      'http://example.com/D',
+      'http://example.com/A',
+      'http://example.com/B',
+      'http://example.com/C',
+    ]);
+  });
+});
