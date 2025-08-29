18b70b058ff014e21471482e5a0358393044c74c
Dylan Van Assche <dylan.vanassche@vlaanderen.be>
2025-08-29 11:57:09 +0200

    oslo-validator-jsonld: improve error message

--- packages/oslo-validator-jsonld/lib/JsonldValidationService.ts
+++ packages/oslo-validator-jsonld/lib/JsonldValidationService.ts
@@ -88,11 +88,11 @@ export class JsonldValidationService implements IService {
 
     if (resultMissingClasses.isValid) {
       this.logger.info(
-        'Validation successful! All referenced classes seem to be included.',
+        'Validation successful! All referenced classes and attributes seem to be included.',
       );
     } else {
       this.logger.info(
-        `Validation found ${resultMissingClasses.invalidEntries.length} missing referenced classes.`,
+        `Validation found ${resultMissingClasses.invalidEntries.length} missing referenced classes or attributes.`,
       );
     }
   }
@@ -387,10 +387,12 @@ export class JsonldValidationService implements IService {
               continue;
             }
 
-            this.logger.error(`Found missing class (${value}): ${uri}`);
+            this.logger.error(
+              `Found missing class or attribute (${value}): ${uri}`,
+            );
             result.invalidEntries.push({
               uri,
-              location: `Class (${value}) is missing: ${uri}`,
+              location: `Class or attribute (${value}) is missing: ${uri}`,
             });
             continue;
           }
@@ -415,10 +417,12 @@ export class JsonldValidationService implements IService {
               continue;
             }
 
-            this.logger.error(`Found missing class (${value}): ${uri}`);
+            this.logger.error(
+              `Found missing class or attribute (${value}): ${uri}`,
+            );
             result.invalidEntries.push({
               uri,
-              location: `Class (${value}) is missing: ${uri}`,
+              location: `Class or attribute (${value}) is missing: ${uri}`,
             });
             continue;
           }
