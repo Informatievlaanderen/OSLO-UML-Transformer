ae3f5698b577be08055603d3148edd6913911027
Dylan Van Assche <dylan.vanassche@vlaanderen.be>
2025-12-01 20:59:29 +0000

    oslo-generator-shacl-template: add pretty Turtle serializer (#196)
    * oslo-generator-shacl-template: add pretty Turtle serializer
    
    Serialize Turtle prettier by grouping blanknodes and use the Turtle list
    shortcut syntax. This improves the readability of SHACL shapes.
    
    * Fix test cases
    
    * Don't use `<rootDir>` for mock files of jest
    
    * use `path.join` instead of `path.resovle`
    
    * jest config runs from the root of the project
    
    * Move mocks to root
    
    * Try without root config
    
    * Remove mocks from root
    
    * Add files via upload
    
    * Added a small comment about why we mocked things
    
    * Bump package version of shacl template generator
    
    ---------
    
    Co-authored-by: Kristof Vandenbroucke <kristof.vandenbrouck@gmail.com>
    Co-authored-by: OSLO-support <OSLO-support@vlaanderen.be>

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,24 @@
 import type { Config } from '@jest/types';
+import path from 'path';
 
-// Or directly use inline config
 const config: Config.InitialOptions = {
-    runner: 'groups',
-    transform: {
-        "^.+\\.ts$": "ts-jest"
-    },
-    testRegex: '/test/.*.test.ts$',
-    moduleFileExtensions: ['ts', 'js'],
-    collectCoverage: true,
-    coveragePathIgnorePatterns: ['/node_modules/', 'index.js'],
-    testEnvironment: 'node',
+  runner: 'groups',
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  // Mock data needed to mock the rdf libraries in Jest.
+  // Issue between module and ESM of the library and the used packages
+  moduleNameMapper: {
+    '^@rdfjs/serializer-turtle$':
+      '<rootDir>/oslo-generator-shacl-template/test/__mocks__/@rdfjs/serializer-turtle.js',
+    '^@rdfjs/sink$':
+      '<rootDir>//oslo-generator-shacl-template/__mocks__/@rdfjs/sink.js',
+  },
+  testRegex: '/test/.*.test.ts$',
+  moduleFileExtensions: ['ts', 'js'],
+  collectCoverage: true,
+  coveragePathIgnorePatterns: ['/node_modules/', 'index.js'],
+  testEnvironment: 'node',
 };
 
 export default config;
