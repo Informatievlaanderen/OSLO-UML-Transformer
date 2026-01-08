3d46b34f1a9ca67cd67783bbb22eb63617fadea3
Dylan Van Assche <dylan.vanassche@vlaanderen.be>
2026-01-08 14:50:01 +0100

    SPARQL Generator (#198)
    * packages: oslo-generator-sparql: add
    
    * oslo-core: namespaces: add CPOV and LOCN
    
    * oslo-core: prefixes: add CPOV and LOCN to fallback
    
    Necessary for SPARQL generator tests in case prefix.cc is offline.
    Also fix the return value of the fallback.
    
    * Bumped `oslo-core`
    
    ---------
    
    Co-authored-by: OSLO-support <OSLO-support@vlaanderen.be>

---
+++ packages/oslo-generator-sparql/index.ts
@@ -0,0 +1,6 @@
+import 'reflect-metadata';
+
+export * from './lib/SparqlGenerationServiceRunner';
+export * from './lib/SparqlGenerationService';
+export * from './lib/config/DependencyInjectionConfig';
+export * from './lib/config/SparqlGenerationServiceConfiguration';
---
+++ packages/oslo-generator-sparql/lib/utils/sparqlUtils.ts
@@ -0,0 +1,37 @@
+import { getPrefixes } from '@oslo-flanders/core';
+import type { SplittedUri } from '../types/Sparql';
+
+export async function splitUri(uri: string): Promise<SplittedUri | undefined> {
+  /* Extract prefix and element by finding the last occurrence of # or / */
+  const hashIndex = uri.lastIndexOf('#');
+  const slashIndex = uri.lastIndexOf('/');
+  const splitIndex = Math.max(hashIndex, slashIndex);
+
+  if (splitIndex > 0) {
+    const prefixUri = uri.slice(0, Math.max(0, splitIndex + 1));
+    const element = uri.replace(prefixUri, '');
+
+    const prefixes = await getPrefixes();
+    let prefixLabel;
+    for (const [label, puri] of Object.entries(prefixes)) {
+      if (puri === prefixUri) {
+        prefixLabel = label;
+        break;
+      }
+    }
+
+    /* No known prefix found for URI */
+    if (!prefixLabel) {
+      return undefined;
+    }
+
+    return {
+      prefix: prefixLabel,
+      uri: prefixUri,
+      element,
+    };
+  }
+
+  /* Splitting failed */
+  return undefined;
+}
