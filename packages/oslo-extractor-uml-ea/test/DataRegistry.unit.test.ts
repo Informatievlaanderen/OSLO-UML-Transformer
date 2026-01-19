d3661398b18096d91312a72acd5d09e354b5dd5b
Dylan Van Assche <dylan.vanassche@vlaanderen.be>
2026-01-19 09:14:52 +0100

    oslo-extractor-uml-ea: tests: DataRegistry: fix test file URI

--- packages/oslo-extractor-uml-ea/test/DataRegistry.unit.test.ts
+++ packages/oslo-extractor-uml-ea/test/DataRegistry.unit.test.ts
@@ -12,7 +12,7 @@ describe('DataRegistry equality', () => {
       new VoidLogger(),
     ).createDataRegistry(
       // eslint-disable-next-line max-len
-      'https://github.com/Informatievlaanderen/OSLO-UML-Transformer/blob/8fa44ec84b901c60a70a64da3d1bb743c55d1aaf/oslo-converter-uml-ea/01-AssociatiesMijnDomein.EAP?raw=true',
+      'https://github.com/Informatievlaanderen/OSLO-UML-Transformer/blob/integration-test-files/oslo-converter-uml-ea/01-AssociatiesMijnDomein.EAP?raw=true',
     );
 
     const sqliteRegistry = await new FileReaderService(
@@ -20,7 +20,7 @@ describe('DataRegistry equality', () => {
       new VoidLogger(),
     ).createDataRegistry(
       // eslint-disable-next-line max-len
-      'https://github.com/Informatievlaanderen/OSLO-UML-Transformer/blob/integration-test-files/oslo-converter-uml-ea/01-AssociatiesMijnDomein-qea.qea?raw=true',
+      'https://github.com/Informatievlaanderen/OSLO-UML-Transformer/raw/refs/heads/integration-test-files/oslo-converter-uml-ea/01-AssociatiesMijnDomein.qea?raw=true',
     );
     
     expect(accessdbRegistry.diagrams.length).toEqual(
