60d9a72e803a5d95db68d3599638f90958b1e583
Dylan Van Assche <dylan.vanassche@vlaanderen.be>
2026-01-30 13:15:21 +0100

    oslo-extractor-uml-ea: fix DataRegistry test

--- packages/oslo-extractor-uml-ea/test/DataRegistry.unit.test.ts
+++ packages/oslo-extractor-uml-ea/test/DataRegistry.unit.test.ts
@@ -12,7 +12,7 @@ describe('DataRegistry equality', () => {
       new VoidLogger(),
     ).createDataRegistry(
       // eslint-disable-next-line max-len
-      'https://github.com/Informatievlaanderen/OSLO-UML-Transformer/blob/8fa44ec84b901c60a70a64da3d1bb743c55d1aaf/oslo-converter-uml-ea/01-AssociatiesMijnDomein.EAP?raw=true',
+      'https://github.com/Informatievlaanderen/OSLO-UML-Transformer/raw/refs/heads/integration-test-files/oslo-converter-uml-ea/01-AssociatiesMijnDomein.EAP',
     );
 
     const sqliteRegistry = await new FileReaderService(
@@ -20,9 +20,9 @@ describe('DataRegistry equality', () => {
       new VoidLogger(),
     ).createDataRegistry(
       // eslint-disable-next-line max-len
-      'https://github.com/Informatievlaanderen/OSLO-UML-Transformer/blob/integration-test-files/oslo-converter-uml-ea/01-AssociatiesMijnDomein-qea.qea?raw=true',
+      'https://github.com/Informatievlaanderen/OSLO-UML-Transformer/raw/refs/heads/integration-test-files/oslo-converter-uml-ea/01-AssociatiesMijnDomein.qea',
     );
-    
+
     expect(accessdbRegistry.diagrams.length).toEqual(
       sqliteRegistry.diagrams.length,
     );
@@ -30,17 +30,17 @@ describe('DataRegistry equality', () => {
       sqliteRegistry.packages.length,
     );
     /* TODO: disable until SQLite variant is updated as well */
-    /*expect(accessdbRegistry.elements.length).toEqual(
-      sqliteRegistry.elements.length,
-    );*/
-    /*expect(accessdbRegistry.connectors.length).toEqual(
-      sqliteRegistry.connectors.length,
-    );
-    expect(accessdbRegistry.attributes.length).toEqual(
-      sqliteRegistry.attributes.length,
-    );
-    expect(accessdbRegistry.crossReferences.length).toEqual(
-      sqliteRegistry.crossReferences.length,
-    );*/
+    // expect(accessdbRegistry.elements.length).toEqual(
+    // sqliteRegistry.elements.length,
+    // );
+    // expect(accessdbRegistry.connectors.length).toEqual(
+    // sqliteRegistry.connectors.length,
+    // );
+    // expect(accessdbRegistry.attributes.length).toEqual(
+    // sqliteRegistry.attributes.length,
+    // );
+    // expect(accessdbRegistry.crossReferences.length).toEqual(
+    // sqliteRegistry.crossReferences.length,
+    // );
   });
 });
