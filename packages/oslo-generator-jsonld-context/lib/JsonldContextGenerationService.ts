a79bbcad5a9e1a5ee713115b076c61f8770aa828
Dylan Van Assche <dylan.vanassche@vlaanderen.be>
2026-04-08 10:30:01 +0200

    fix: oslo-generator-jsonld-context: pass logger to findAllAttribute
    Logger needs to be passed now as part of the function call.
    
    Co-Developed-By: Kristof Vandenbroucke <kristof.vandenbroucke@vlaandere.be>

--- packages/oslo-generator-jsonld-context/lib/JsonldContextGenerationService.ts
+++ packages/oslo-generator-jsonld-context/lib/JsonldContextGenerationService.ts
@@ -140,7 +140,7 @@ export class JsonldContextGenerationService implements IService {
               return y.osloId;
             });
         } else {
-          attributeIds = findAllAttributes(x.osloId, attributeIds, this.store);
+          attributeIds = findAllAttributes(x.osloId, attributeIds, this.store, this.logger);
         }
 
         return {
