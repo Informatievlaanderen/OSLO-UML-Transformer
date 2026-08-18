e2cf88e5dc2329c6da353bd1a16baa973448efa3
Dylan Van Assche <dylan.vanassche@vlaanderen.be>
2026-08-18 08:57:03 +0200

    fix(oslo-generator-swagger): add Abstract Class tests

--- packages/oslo-generator-swagger/test/SwaggerGenerationService.unit.test.ts
+++ packages/oslo-generator-swagger/test/SwaggerGenerationService.unit.test.ts
@@ -451,4 +451,17 @@ describe('SwaggerGenerationService', () => {
     // Cleanup
     rmSync('output-withlinks', { recursive: true, force: true });
   });
+
+  it('should not include abstract class in endpoints', async () => {
+    await service.store.addQuads(await parseJsonld(kvsInput));
+    await service.run();
+
+    const swagger = JSON.parse(
+      readFileSync('output/swagger/example.json').toString(),
+    );
+
+    // Abstract class
+    expect(swagger.components.schemas.Agent).toBeUndefined();
+    expect(Object.keys(swagger.paths).includes('Agent')).toBeFalsy();
+  });
 });
